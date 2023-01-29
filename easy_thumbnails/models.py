ddc15848bc6b783b98c7bf22d6d08196e4c817df
James Tiplady <biggleszx@biggleszx.com>
2023-01-29 22:22:04 +0000

    Implementing URL and queryset caching

--- easy_thumbnails/models.py
+++ easy_thumbnails/models.py
@@ -1,16 +1,42 @@
+from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from django.utils import timezone
 
 from easy_thumbnails import utils, signal_handlers
 from easy_thumbnails.conf import settings
+from easy_thumbnails.version_utils import get_version
+
+
+if settings.THUMBNAIL_CACHE:
+    try:
+        from django.core.cache import caches
+        cache = caches[settings.THUMBNAIL_CACHE]
+    except ImportError:
+        raise ImproperlyConfigured(
+            'You are trying to use the cache on a version of Django '
+            'that does not support caching')
+    except KeyError:
+        raise ImproperlyConfigured(
+            "The cache specified in `THUMBNAIL_CACHE` doesn't seem "
+            "to exist")
 
 
 class FileManager(models.Manager):
 
+    def _get_cache_key(self, kwargs):
+        version = get_version()
+        return f"easy_thumbnails:{version}:FileManager:{kwargs['storage_hash']}:{kwargs['name']}"
+
+    def _delete_cache_key(self, kwargs):
+        cache_key = self._get_cache_key(kwargs)
+        cache.delete(cache_key)
+
     def get_file(self, storage, name, create=False, update_modified=None,
                  check_cache_miss=False, **kwargs):
         kwargs.update(dict(storage_hash=utils.get_storage_hash(storage),
-                           name=name))
+                           name=name))        
+        cache_key = self._get_cache_key(kwargs)
+
         if create:
             if update_modified:
                 defaults = kwargs.setdefault('defaults', {})
@@ -19,22 +45,34 @@ class FileManager(models.Manager):
         else:
             created = False
             kwargs.pop('defaults', None)
-            try:
-                manager = self._get_thumbnail_manager()
-                obj = manager.get(**kwargs)
-            except self.model.DoesNotExist:
 
-                if check_cache_miss and storage.exists(name):
-                    # File already in storage, update cache. Using
-                    # get_or_create again in case this was updated while
-                    # storage.exists was running.
-                    obj, created = self.get_or_create(**kwargs)
-                else:
-                    return
+            obj = None
+            if settings.THUMBNAIL_CACHE and settings.THUMBNAIL_QUERYSET_CACHING:
+                obj = cache.get(cache_key)
+            if obj is None:
+                try:
+                    manager = self._get_thumbnail_manager()
+                    obj = manager.get(**kwargs)
+                except self.model.DoesNotExist:
+
+                    if check_cache_miss and storage.exists(name):
+                        # File already in storage, update cache. Using
+                        # get_or_create again in case this was updated while
+                        # storage.exists was running.
+                        obj, created = self.get_or_create(**kwargs)
+                    else:
+                        return
+
+                if settings.THUMBNAIL_CACHE and settings.THUMBNAIL_QUERYSET_CACHING:
+                    cache.set(cache_key, obj, None)
 
         if update_modified and not created:
             if obj.modified != update_modified:
                 self.filter(pk=obj.pk).update(modified=update_modified)
+
+                if settings.THUMBNAIL_CACHE and settings.THUMBNAIL_QUERYSET_CACHING:
+                    obj.modified = update_modified
+                    cache.set(cache_key, obj, None)
 
         return obj
 
@@ -43,6 +81,10 @@ class FileManager(models.Manager):
 
 
 class ThumbnailManager(FileManager):
+    
+    def _get_cache_key(self, kwargs):
+        version = get_version()
+        return f"easy_thumbnails:{version}:ThumbnailManager:{kwargs['storage_hash']}:{kwargs['name']}:{kwargs['source'].pk}"
 
     def _get_thumbnail_manager(self):
         if settings.THUMBNAIL_CACHE_DIMENSIONS:
